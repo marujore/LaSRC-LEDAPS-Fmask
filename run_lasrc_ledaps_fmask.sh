5cc7a583c615a4d602822b62c25315c3d5c77654
marujore <rennanmarujo@gmail.com>
2020-06-02 16:28:11 -0300

    Create Output folder using sceneid

--- run_lasrc_ledaps_fmask.sh
+++ run_lasrc_ledaps_fmask.sh
@@ -20,8 +20,8 @@ if [ -z "${INDIR}" ]; then
     INDIR=/mnt/input-dir
 fi
 
-if [ -z "${INDIR}" ]; then
-    OUTDIR=/mnt/output-dir
+if [ -z "${OUTDIR}" ]; then
+    OUTDIR=/mnt/output-dir/
 fi
 
 
@@ -69,6 +69,7 @@ if [[ $1 == "LT04"* ]] || [[ $1 == "LT05"* ]] || [[ $1 == "LE07"* ]] || [[ $1 ==
     /usr/GERS/Fmask_4_2/application/run_Fmask_4_2.sh $MCROOT "$@"
 
     ## Copy outputs from workdir
+    OUTDIR=${OUTDIR}/${SCENE_ID}
     mkdir -p $OUTDIR
     OUT_PATTERNS="$WORKDIR/${SCENE_ID}_toa_*.tif $WORKDIR/${SCENE_ID}_sr_*.tif $WORKDIR/${SCENE_ID}_bt_*.tif $WORKDIR/${SCENE_ID}_radsat_qa.tif $WORKDIR/${SCENE_ID}_sensor*.tif $WORKDIR/${SCENE_ID}_solar*.tif"
     for f in $OUT_PATTERNS; do
@@ -78,7 +79,6 @@ if [[ $1 == "LT04"* ]] || [[ $1 == "LT05"* ]] || [[ $1 == "LE07"* ]] || [[ $1 ==
     for f in $OUT_PATTERNS; do
         cp $f $OUTDIR/${SCENE_ID}_Fmask4.tif
     done
-
     for f in $MTD_FILES; do
         cp $WORKDIR/$(basename $f) $OUTDIR/$(basename $f)
     done
@@ -92,7 +92,6 @@ elif [[ $1 == "S2"* ]]; then
     SAFEDIR=/mnt/input-dir/${SAFENAME}
     SCENE_ID=${SAFENAME:0:-5}
     WORKDIR=/work/${SAFENAME}
-    OUTDIR=/mnt/output-dir/
     JP2_PATTERNS=$(find ${INDIR} -name "${SCENE_ID}_*.jp2" -o -name "${SCENE_ID}_*.JP2")
 
 
@@ -121,13 +120,13 @@ elif [[ $1 == "S2"* ]]; then
     done
     do_lasrc_sentinel.py --xml=${SCENE_ID_XML}
     convert_espa_to_gtif --xml=${SCENE_ID_XML} --gtif=${SCENE_ID} --del_src_files
-
     ##FMASK
     MCROOT=/usr/local/MATLAB/MATLAB_Runtime/v96
     cd ${GRANULE_SCENE}
     /usr/GERS/Fmask_4_2/application/run_Fmask_4_2.sh $MCROOT "$@"
 
     ## Copy outputs from workdir
+    OUTDIR=${OUTDIR}/${SCENE_ID}
     mkdir -p $OUTDIR
     OUT_PATTERNS="${IMG_DATA}/${SCENE_ID}_sr_*.tif"
     for f in $OUT_PATTERNS; do
